95b6447590048ded13ea23687430b5649d8cbca2
Curtis Larsen <curtis.gale.larsen@gmail.com>
2020-09-16 10:50:06 -0600

    example

---
+++ code/y20m09d14p01-conditional/main.cpp
@@ -0,0 +1,43 @@
+
+
+int main() {
+  int a, b, c, d;
+  bool x;
+
+  if(a < b) {
+  } else if(a >= b && c < d) {
+  } else if(a < b || c == d) {
+  } else if(x) {
+  } else if(!x) {
+  } else if(a-b) {
+  } else if(!(a-b)) {
+  } else {
+  }
+  // the test is an expression
+  // expression is evaluated
+  // its value is interpreted as true or false
+  // bool values are actually true or false
+  // int values are true if they are not 0, false if they are 0
+  // double values are true if they are not 0, false if they are 0
+  // stay tuned for objects
+  // !=, ==, <, <=, >, >=
+  // &&, ||, !
+
+
+  while(a < b) {
+    a++;
+  }
+  
+  // ++ adds 1 to integers
+  // += 2
+  int i;
+  for(i = 0; i < a; i++) {
+  }
+  // value of i
+
+  // prefer not to declare in the for loop
+  // for(int i = 0; i < a; i++) {
+  // }
+
+  return 0;
+}
